8102055f2c6a143b14c2d364340bf06e42377aa4
Sergey-dotcom <70769015+Sergey-dotcom@users.noreply.github.com>
2020-12-29 13:25:48 +0300

    feat: update style.css add main.js

---
+++ main.js
@@ -0,0 +1,174 @@
+const weatherIcons = {
+  800: {
+    c01d: "assets/img/wheather-icons/clear-day.svg",
+    c01n: "assets/img/wheather-icons/clear-night.svg",
+  },
+  804: {
+    c04d: "assets/img/wheather-icons/cloudy.svg",
+    c04n: "assets/img/wheather-icons/cloudy.svg",
+  },
+  301: {
+    d02d: "assets/img/wheather-icons/drizzle.svg",
+    d02n: "assets/img/wheather-icons/drizzle.svg",
+  },
+  233: {
+    t05d: "assets/img/wheather-icons/hail.svg",
+    t05n: "assets/img/wheather-icons/hail.svg",
+  },
+  700: {
+    a01d: "assets/img/wheather-icons/mist.svg",
+    a01n: "assets/img/wheather-icons/mist.svg",
+  },
+  801: {
+    c02d: "assets/img/wheather-icons/partly-cloudy-day.svg",
+    c02n: "assets/img/wheather-icons/partly-cloudy-night.svg",
+  },
+  802: {
+    c02d: "assets/img/wheather-icons/partly-cloudy-day.svg",
+    c02n: "assets/img/wheather-icons/partly-cloudy-night.svg",
+  },
+  521: {
+    r05d: "assets/img/wheather-icons/partly-cloudy-day-drizzle.svg",
+    r05n: "assets/img/wheather-icons/partly-cloudy-night-drizzle.svg",
+  },
+  511: {
+    f01d: "assets/img/wheather-icons/partly-cloudy-day-hail.svg",
+    f01n: "assets/img/wheather-icons/partly-cloudy-night-hail.svg",
+  },
+  520: {
+    r04d: "assets/img/wheather-icons/partly-cloudy-day-rain.svg",
+    r04n: "assets/img/wheather-icons/partly-cloudy-night-rain.svg",
+  },
+  600: {
+    s01d: "assets/img/wheather-icons/partly-cloudy-day-snow.svg",
+    s01n: "assets/img/wheather-icons/partly-cloudy-night-snow.svg",
+  },
+  522: {
+    r06d: "assets/img/wheather-icons/rain.svg",
+    r06n: "assets/img/wheather-icons/rain.svg",
+  },
+  502: {
+    r03d: "assets/img/wheather-icons/rain.svg",
+    r03n: "assets/img/wheather-icons/rain.svg",
+  },
+  500: {
+    r01d: "assets/img/wheather-icons/rain.svg",
+    r01n: "assets/img/wheather-icons/rain.svg",
+  },
+  501: {
+    r02d: "assets/img/wheather-icons/rain.svg",
+    r02n: "assets/img/wheather-icons/rain.svg",
+  },
+  601: {
+    s02d: "assets/img/wheather-icons/snow.svg",
+    s02n: "assets/img/wheather-icons/snow.svg",
+  },
+  602: {
+    s03d: "assets/img/wheather-icons/snow.svg",
+    s03n: "assets/img/wheather-icons/snow.svg",
+  },
+  611: {
+    s05d: "assets/img/wheather-icons/snow.svg",
+    s05n: "assets/img/wheather-icons/snow.svg",
+  },
+  621: {
+    s01d: "assets/img/wheather-icons/snow.svg",
+    s01n: "assets/img/wheather-icons/snow.svg",
+  },
+  622: {
+    s02d: "assets/img/wheather-icons/snow.svg",
+    s02n: "assets/img/wheather-icons/snow.svg",
+  },
+  200: {
+    to1d: "assets/img/wheather-icons/thunderstorms.svg",
+    to1n: "assets/img/wheather-icons/thunderstorms.svg",
+  },
+  201: {
+    t02d: "assets/img/wheather-icons/thunderstorms.svg",
+    t02n: "assets/img/wheather-icons/thunderstorms.svg",
+  },
+  202: {
+    t03d: "assets/img/wheather-icons/thunderstorms.svg",
+    t03n: "assets/img/wheather-icons/thunderstorms.svg",
+  },
+  230: {
+    t04d: "assets/img/wheather-icons/thunderstorms.svg",
+    t04n: "assets/img/wheather-icons/thunderstorms.svg",
+  },
+  231: {
+    t04d: "assets/img/wheather-icons/thunderstorms.svg",
+    t04n: "assets/img/wheather-icons/thunderstorms.svg",
+  },
+  232: {
+    t04d: "assets/img/wheather-icons/thunderstorms.svg",
+    t04n: "assets/img/wheather-icons/thunderstorms.svg",
+  },
+};
+
+const translations = {
+    "en": 
+    {
+        "searchInput": "Search city or ZIP",
+        "searchButton": "search",
+        "apparentTemperature": "feels like:",
+        "windSpeed": "wind speed::",
+        "airHumidity": "Humidity:"
+    },
+
+"ru":
+    {
+        "searchInput": "Найти город или индекс",
+        "searchButton": "поиск",
+        "apparentTemperature": "ощущается как:",
+        "windSpeed": "скорость ветра:",
+        "airHumidity": "влажность:"
+    },
+    
+"be": 
+    {
+        "searchInput": "Знайсці горад ці індэкс",
+        "searchButton": "пошук",
+        "apparentTemperature": "адчуваецца як:",
+        "windSpeed": "хуткасць ветру:",
+        "airHumidity": "вільготнасць:",
+        "days": 
+            {
+                "long":
+                    [
+                        'нядзеля', 
+                        'панядзелак', 
+                        'аўторак', 
+                        'серада', 
+                        'чацвер', 
+                        'пятніца', 
+                        'субота'
+                    ],
+                "short":
+                    [
+                        'няд', 
+                        'пнд', 
+                        'аўт', 
+                        'сер', 
+                        'чцв', 
+                        'пят', 
+                        'суб'
+                    ]
+            },
+            
+        "month": 
+            [
+                'Студзень', 
+                'Люты', 
+                'Сакавiк', 
+                'Красавiк', 
+                'Май', 
+                'Чэрвень', 
+                'Лiпень', 
+                'Жнiвень', 
+                'Верасень', 
+                'Кастрычнiк', 
+                'Лiстапад', 
+                'Снежань'
+            ]
+    }
+}
